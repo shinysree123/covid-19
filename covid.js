35ddd87f756a72a970d94533cc79560b6e4aa1da
shinysree123 <shinyjoseph89@gmail.com>
2020-07-30 10:56:33 +0530

    eror corected

--- covid.js
+++ covid.js
@@ -3,9 +3,9 @@ var Parser=require('body-parser');
 var Mongoose=require('mongoose');
 var app=Express();
 var Coviddetailsrouter=require('./router/detailrouter');
-app.use('/',Coviddetailsrouter);
+app.use('/patient',Coviddetailsrouter);
 var Testrouter=require('./router/testrouter');
-app.use('/',Testrouter);
+app.use('/test',Testrouter);
 Mongoose.connect("mongodb+srv://shinyjoseph:shiny@cluster0-wburb.mongodb.net/test?retryWrites=true&w=majority");
 app.use(Parser.urlencoded({extended:false}));
 app.listen(process.env.PORT||4000,()=>{
